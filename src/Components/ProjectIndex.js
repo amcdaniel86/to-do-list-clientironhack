8f41e03af0ad2d141e3fe32f53f4addcc40d4ab6
Alex McDaniel <amcdaniel186@gmail.com>
2018-12-07 12:08:01 -0500

    initupdate

---
+++ src/Components/ProjectIndex.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+ipmort Axios from 'axios';
+
+
+
+
+class ProjectIndex extends Component {
+
+      render(){
+          return(
+              <div>this is project index</div>
+
+
+
+
+          )
+      }
+
+
+
+
+}
+
+
+
+
+
+
+export default ProjectIndex;
